5f8071b0adeaf8e03d5c3cebdf5134c17342e430
Jackson Zhipeng Chang <zchang0302@gmail.com>
2018-06-12 09:03:01 -0600

    fix admin bug

--- webhook_emailer/controllers/admin.py
+++ webhook_emailer/controllers/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import NotificationTemplate
 from .models import Webhook
-from .models import Initiative
+from .models import RequestValue
 
 
 # Register your models here.
 
 admin.site.register(NotificationTemplate)
 admin.site.register(Webhook)
-admin.site.register(Initiative)
+admin.site.register(RequestValue)
