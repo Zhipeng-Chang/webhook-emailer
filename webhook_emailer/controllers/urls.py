7719dc7404687b65268409a19ea971724637babd
Zhipeng-Chang <zchang0302@gmail.com>
2018-06-19 15:35:03 -0600

    add admin page to index

--- webhook_emailer/controllers/urls.py
+++ webhook_emailer/controllers/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 admin.autodiscover()
 urlpatterns =[
+    path('', admin.site.urls, name='index'),
     path('webhook/url/', views.webhook_catch_octava, name='webhook-catch')
 
 ]
